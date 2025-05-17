a9aff71f78abf11c106f2225c5b2ba72b8b67170
Fahiye Muhammad <fahiye254@gmail.com>
2025-05-17 17:17:53 +0300

    Learning intro to SQLite by creating a table , updating it and writing a query that retrieves data with certain conditions

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT ,
+  age INTEGER,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN,
+  sex TEXT
+);
---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears (name, age, color , temperament,alive,sex)
+VALUES 
+
+  ('Mr. Chocolate', 10, 'brown', 'gentle', TRUE, 'M'),
+  ('Rowdy', 1, 'blue', 'visious', TRUE, 'M'),
+  ('Tabitha', 11, 'brown', 'gentle', FALSE, 'F'),
+  ('Sergent Brown', 8, 'brown', 'gentle', TRUE, 'F'),
+  ('Melissa', 6, 'brown', 'gentle', TRUE, 'M'),
+  ('Grinch', 8, 'brown', 'gentle', TRUE, 'M'),
+  ('Wendy', 10, 'brown', 'gentle', TRUE, 'F'),
+  (NULL, 3, 'brown', 'gentle', TRUE, 'M');
+
+
